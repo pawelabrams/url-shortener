b89c429bb20f97dbeeb166ec35014acb8e9e35d6
Paweł Abramowicz <pawel@abramowicz.org>
2021-04-18 00:31:27 +0200

    Basic functionality.

---
+++ api/src/Document/Shorten.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Document;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ODM\Document
+ */
+#[ApiResource]
+class Shorten
+{
+    /**
+     * @ODM\Id(strategy="ALNUM",options={"chars"="0123456789BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz","pad"=6})
+     */
+    private ?string $id = null;
+
+    /**
+     * @ODM\Field
+     */
+    #[Assert\NotBlank]
+    #[Assert\Url(message: 'The URL must be valid and start with either http or https protocol.')]
+    // TODO: consider adding relativeProtocol: true above to allow protocol-relative shortens?
+    public string $url = '';
+
+    /**
+     * @ODM\Field(type="int")
+     */
+    private int $visitors = 0;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getVisitors(): int
+    {
+        return $this->visitors;
+    }
+}
---
+++ api/src/Operation/SingularPathSegmentNameGenerator.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Operation;
+
+use ApiPlatform\Core\Operation\PathSegmentNameGeneratorInterface;
+
+class SingularPathSegmentNameGenerator implements PathSegmentNameGeneratorInterface
+{
+    /**
+     * Transforms a given string to a valid path name which will not be pluralized.
+     *
+     * @param string $name usually a ResourceMetadata shortname
+     *
+     * @return string A string that is a part of the route name
+     */
+    public function getSegmentName(string $name, bool $collection = true): string
+    {
+        return strtolower(preg_replace('~(?<=\\w)([A-Z])~', '-$1', $name));
+    }
+}
