393a064fe416026429ca8000877f1cdb5e322327
Paweł Abramowicz <pawel@abramowicz.org>
2021-04-18 18:48:42 +0200

    Added tests. Fixed bug: no '/api' segment in api endpoints.

---
+++ api/tests/Api/RedirectTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\Api;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Document\Shorten;
+
+/**
+ * @covers \App\Controller\Redirect
+ */
+class RedirectTest extends ApiTestCase
+{
+    public function testRedirection(): void
+    {
+        $client = static::createClient();
+
+        /** Permanent redirect when accessing URL: HTTP 308 */
+        $this->createShortenedUrl($client);
+
+        // Find internal resource id (eg. "/api/shorten/00001P")
+        $iri = $this->findIriBy(Shorten::class, ['url' => 'https://redirect-test.example']);
+        $shortenId = str_replace('/api/shorten/', '', $iri);
+        $this->assertNotEmpty($shortenId);
+
+        $response = $client->request('GET', sprintf('/%s', $shortenId));
+        $this->assertResponseStatusCodeSame(308);
+        $this->assertResponseHeaderSame('location', 'https://redirect-test.example');
+
+        $response = $client->request('GET', $iri);
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonContains([
+            'url' => 'https://redirect-test.example',
+            'visitors' => 1,
+        ]);
+
+        /** Non-existent id: Redirect temporarily to a 404 page in PWA, HTTP 307 */
+        $response = $client->request('GET', '/000000');
+        $this->assertResponseStatusCodeSame(307);
+        $this->assertResponseHeaderSame('location', '/404');
+    }
+
+    /**
+     * Create a shortened URL not to depend on ShortenTest
+     */
+    private function createShortenedUrl($client): void
+    {
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'url' => 'https://redirect-test.example',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(201);
+    }
+}
+
--- api/tests/Api/ShortenTest.php
+++ api/tests/Api/ShortenTest.php
@@ -1,8 +1,10 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Tests\Api;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Document\Shorten;
 
 /**
  * @covers \App\Document\Shorten
@@ -11,15 +13,126 @@ class ShortenTest extends ApiTestCase
 {
     public function testCreateShortenedUrl()
     {
-        $response = static::createClient()->request('POST', '/shorten', ['json' => [
+        $client = static::createClient();
+
+        /** HTTPS is OK */
+        $response = $client->request('POST', '/api/shorten', ['json' => [
             'url' => 'https://test.example',
         ]]);
 
         $this->assertResponseStatusCodeSame(201);
+        $this->assertMatchesResourceItemJsonSchema(Shorten::class);
         $this->assertJsonContains([
-            '@context' => '/contexts/Shorten',
-            '@type' => 'Shorten',
             'url' => 'https://test.example',
+            'visitors' => 0,
         ]);
+
+        /** HTTP is OK */
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'url' => 'http://test.example',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertMatchesResourceItemJsonSchema(Shorten::class);
+        $this->assertJsonContains([
+            'url' => 'http://test.example',
+            'visitors' => 0,
+        ]);
+
+        /** FTP: 422 */
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'url' => 'ftp://test.example',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(422);
+
+        /** Websockets: 422 */
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'url' => 'wss://test.example',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(422);
+
+        /** No protocol: 422 */
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'url' => 'test.example',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(422);
+
+        /** Bad field name: 422 */
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'link' => 'https://test.example',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(422);
+
+        /** Null in field: 400 */
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'url' => null,
+        ]]);
+
+        $this->assertResponseStatusCodeSame(400);
+
+        /** Number in field: 400 */
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'url' => 1,
+        ]]);
+
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    /**
+     * @depends testCreateShortenedUrl
+     */
+    public function testViewShortenedUrl()
+    {
+        $client = static::createClient();
+
+        // Find internal resource id (eg. "/api/shorten/00001P")
+        $iri = $this->findIriBy(Shorten::class, ['url' => 'https://test.example']);
+
+        $client->request('GET', $iri);
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testViewCollectionOfShortenedUrls()
+    {
+        $response = static::createClient()->request('GET', '/api/shorten');
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertMatchesResourceCollectionJsonSchema(Shorten::class);
+    }
+
+    public function testDeleteShortenedUrl()
+    {
+        $client = static::createClient();
+
+        $response = $client->request('POST', '/api/shorten', ['json' => [
+            'url' => 'https://delete-test.example',
+        ]]);
+
+        // Find internal resource id (eg. "/api/shorten/00001P")
+        $iri = $this->findIriBy(Shorten::class, ['url' => 'https://delete-test.example']);
+
+        $response = $client->request('DELETE', $iri);
+        $this->assertResponseStatusCodeSame(204);
+        $this->assertEmpty($response->getContent());
+
+        /** Wrong id: 404 */
+        $response = $client->request('DELETE', '/api/shorten/0001');
+        $this->assertResponseStatusCodeSame(404);
+
+        /** Zero id: 404 */
+        $response = $client->request('DELETE', '/api/shorten/0');
+        $this->assertResponseStatusCodeSame(404);
+
+        /** No id: 404 */
+        $response = $client->request('DELETE', '/api/shorten/');
+        $this->assertResponseStatusCodeSame(404);
+
+        /** Collection URL used: 405 */
+        $response = $client->request('DELETE', '/api/shorten');
+        $this->assertResponseStatusCodeSame(405);
     }
 }
